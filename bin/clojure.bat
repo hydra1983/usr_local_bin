1afd3e2db9c68e17a0f35fbd691b93a12b47a243
Edison Guo <hydra1983@gmail.com>
2013-12-12 18:30:02 +0800

    remove clojure.bat

--- bin/clojure.bat
+++
@@ -1,3 +0,0 @@
-@ECHO off
-SET PATH=%SystemRoot%\system32;%SystemRoot%;%SystemRoot%\System32\Wbem;D:\usr\local\bin;
-"%JAVA_HOME%\bin\java.exe" -cp "D:\usr\share\clojure-1.5.1\clojure-1.5.1.jar" clojure.main
