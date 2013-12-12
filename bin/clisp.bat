ed8dc4e2652bb7ac15e0834cde55bfa445dc2d30
Edison Guo <hydra1983@gmail.com>
2013-12-12 18:21:11 +0800

    add clojure and lisp and python

---
+++ bin/clisp.bat
@@ -0,0 +1,3 @@
+@ECHO off
+SET PATH=%SystemRoot%\system32;%SystemRoot%;%SystemRoot%\System32\Wbem;D:\usr\local\bin;
+"D:\usr\share\clisp-2.49\clisp" %*
---
+++ bin/python.bat
@@ -0,0 +1,3 @@
+@ECHO off
+SET PATH=%SystemRoot%\system32;%SystemRoot%;%SystemRoot%\System32\Wbem;D:\usr\local\bin;
+"D:\usr\share\python-2.7.3\python" %*
