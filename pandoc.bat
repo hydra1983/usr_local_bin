8ca67ad78565c0a4f7988925d79f2903e30e52a2
Edison Guo <hydra1983@gmail.com>
2013-12-28 17:18:26 +0800

    add fc-list and pandoc

---
+++ pandoc.bat
@@ -0,0 +1,3 @@
+@ECHO off
+SET PATH=%SystemRoot%\system32;%SystemRoot%;%SystemRoot%\System32\Wbem;D:\usr\local\bin;C:\Program Files (x86)\MiKTeX 2.9\miktex\bin;
+"C:\Users\Edison\AppData\Local\Pandoc\pandoc" %*
--- rasterize.bat
+++ rasterize.bat
@@ -1,3 +1,3 @@
 @ECHO off
 SET PATH=%SystemRoot%\system32;%SystemRoot%;%SystemRoot%\System32\Wbem;D:\usr\local\bin;
-"D:\usr\local\lib\phantomjs-1.9.1-windows\phantomjs" --output-encoding=GBK D:\usr\local\lib\phantomjs-1.9.1-windows\examples\rasterize.js %*
+"D:\usr\local\lib\phantomjs-1.9.2-windows\phantomjs" --output-encoding=GBK D:\usr\local\lib\phantomjs-1.9.2-windows\examples\rasterize.js %*
