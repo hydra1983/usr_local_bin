583a7d83657f7844df80c6b75473c3b2310f422a
hydra1983 <hydra1983@gmail.com>
2014-01-02 02:30:47 +0800

    set path automatically

--- .paths/mk_path.js
+++ .paths/mk_path.js
@@ -1,7 +1,9 @@
 var fs = require('fs');
 var os = require('os');
 
-const ENCODING = "utf-8";
+const ENCODING = 'utf-8';
+const HOME = process.env['home'];
+console.log(HOME);
 
 var data;
 
@@ -12,53 +14,69 @@ try{
 }
 
 if(data == null){
-	data = "";
+	data = '';
 }
 
 data = String(data);
 
-if(data == ""){
+if(data == ''){
 	process.exit(0);
 }
 
 var lines = data.split(/\r\n|\r|\n/g);
 var n = lines.length;
-var win_path = "";
-var linux_path = "";
+var win_path = '';
+var linux_path = '';
 var trim = function(string){
-	return string.replace(/(^\s+)|(\s+$)/g,"");
+	return string.replace(/(^\s+)|(\s+$)/g,'');
 };
 var getWinPath = function(path){
 	return path;
 };
 
 var getLinuxPath = function(path){
-	path = path.replace(/\\/g,"/");
-	path = path.replace(/([a-zA-Z]):/,"/$1");
-	path = path.replace(/\s/g,"\\ ");
-	path = path.replace(/\(/g,"\\(");
-	path = path.replace(/\)/g,"\\)");
+	path = path.replace(/\\/g,'/');
+	path = path.replace(/([a-zA-Z]):/,'/$1');
+	path = path.replace(/\s/g,'\\ ');
+	path = path.replace(/\(/g,'\\(');
+	path = path.replace(/\)/g,'\\)');
 	return path;
 };
 
 for(var i = 0;i < n;i++){
 	var line = trim(lines[i]);
-	if(line == "" || line.indexOf("#") == 0){
+	if(line == '' || line.indexOf('#') == 0){
 		continue;
 	}
 
 	if(i != n - 1){
-		win_path += getWinPath(line) + ";";
-		linux_path += getLinuxPath(line) + ":";
+		win_path += getWinPath(line) + ';';
+		linux_path += getLinuxPath(line) + ':';
 	} else {
 		win_path += getWinPath(line) ;
 		linux_path += getLinuxPath(line);
 	}		
 }
-console.log("########## [WINDOWS] ##########");
+
+linux_path = '.:/usr/local/bin:/mingw/bin:/bin:' + linux_path;
+
+console.log('########## [WINDOWS] ##########');
 console.log(win_path);
-console.log("########## [LINUX] ##########");
+console.log('########## [LINUX] ##########');
 console.log(linux_path);
 
-fs.writeFileSync(".win_path",win_path,{encoding:ENCODING});
-fs.writeFileSync(".linux_path",linux_path,{encoding:ENCODING});
+fs.writeFileSync('.win_path',win_path,{encoding:ENCODING});
+fs.writeFileSync('.linux_path',linux_path,{encoding:ENCODING});
+
+// set current user PATH
+var exec = require('child_process').exec;
+var child = exec('setx Path ' + '"' + win_path + '"',function(error, stdout, stderr){
+	if (error !== null) {
+      	console.log('[ERROR]: ' + error);
+    } else {
+    	console.log('[EXEC]' + stdout);
+    };
+});
+
+// set git bash PATH
+fs.writeFileSync(HOME + '/.profile','export PATH=' + linux_path,{encoding:ENCODING});
