115fbdb2dfde391d8cbe7f3aa150145bd18895d4
Edison Guo <hydra1983@gmail.com>
2014-01-02 16:43:03 +0800

    config env vars

--- env/mkenv.js
+++ env/mkenv.js
@@ -3,33 +3,11 @@ var os = require('os');
 
 const ENCODING = 'utf-8';
 const HOME = process.env['home'];
-console.log(HOME);
 
-var data;
-
-try{
-	data = fs.readFileSync(__dirname + '/paths',{encoding:ENCODING});
-} catch(error) {
-	throw error;
-}
-
-if(data == null){
-	data = '';
-}
-
-data = String(data);
-
-if(data == ''){
-	process.exit(0);
-}
-
-var lines = data.split(/\r\n|\r|\n/g);
-var n = lines.length;
-var win_path = '';
-var linux_path = '';
 var trim = function(string){
-	return string.replace(/(^\s+)|(\s+$)/g,'');
-};
+		return string.replace(/(^\s+)|(\s+$)/g,'');
+	};
+
 var getWinPath = function(path){
 	return path;
 };
@@ -43,40 +21,123 @@ var getLinuxPath = function(path){
 	return path;
 };
 
-for(var i = 0;i < n;i++){
-	var line = trim(lines[i]);
-	if(line == '' || line.indexOf('#') == 0){
-		continue;
+var readLines = function(path, opts){
+	if(opts == null){
+		opts = {};
+		opts.encoding = ENCODING;
 	}
 
-	if(i != n - 1){
-		win_path += getWinPath(line) + ';';
-		linux_path += getLinuxPath(line) + ':';
+	var data;
+	try{
+		data = fs.readFileSync(path, opts);
+	} catch(error) {
+		throw error;
+	}
+
+	if(data == null){
+		data = '';
+	}
+
+	data = String(data);
+
+	var lines = [];
+	var rawLines = data.split(/\r\n|\r|\n/g);
+	var n = rawLines.length;
+	for(var i = 0 ; i < n ; i++){
+		var line = trim(rawLines[i]);
+		if(line == '' || line.indexOf('#') == 0){
+			continue;
+		}
+		lines.push(line);
+
+	}
+
+	return lines;
+};
+
+var setenv = function(name, value, isGlobal){
+	var exec = require('child_process').exec;
+	var cmd = '"' + __dirname + '\\setenv.exe" ';
+	if(isGlobal){
+		cmd += '-a ';
 	} else {
-		win_path += getWinPath(line) ;
-		linux_path += getLinuxPath(line);
-	}		
-}
+		cmd += '-ua ';
+	}
+	cmd += name + ' ';
+	cmd += '"' + value + '"';
+	var child = exec(cmd, function(error, stdout, stderr){
+		if (error !== null) {
+			if(!(error.toString().indexOf('The system cannot find the path specified') > -1)){
+				console.log('[ERROR]: ' + error);
+			}
+	    } else {
+	    	console.log('[EXEC]' + stdout);
+	    };
+	});
+};
 
-linux_path = '.:/usr/local/bin:/mingw/bin:/bin:' + linux_path;
+(function(){
+	var lines = readLines(__dirname + '/paths');
+	var n = lines.length;
 
-console.log('########## [WINDOWS] ##########');
-console.log(win_path);
-console.log('########## [LINUX] ##########');
-console.log(linux_path);
+	var win_path = '';
+	var linux_path = '';
 
-fs.writeFileSync('.win_path',win_path,{encoding:ENCODING});
-fs.writeFileSync('.linux_path',linux_path,{encoding:ENCODING});
+	for(var i = 0;i < n;i++){
+		var line = trim(lines[i]);
+		if(i != n - 1){
+			win_path += getWinPath(line) + ';';
+			linux_path += getLinuxPath(line) + ':';
+		} else {
+			win_path += getWinPath(line) ;
+			linux_path += getLinuxPath(line);
+		}		
+	}
+	
+	// win
+	console.log('########## [Set Windows Path] ##########');
+	console.log(win_path);
+	fs.writeFileSync('.win_path',win_path,{encoding:ENCODING});
+	setenv('Path', win_path);
+	console.log();
 
-// set current user PATH
-var exec = require('child_process').exec;
-var child = exec('setx Path ' + '"' + win_path + '"',function(error, stdout, stderr){
-	if (error !== null) {
-      	console.log('[ERROR]: ' + error);
-    } else {
-    	console.log('[EXEC]' + stdout);
-    };
-});
+	// linux
+	linux_path = '.:/usr/local/bin:/mingw/bin:/bin:' + linux_path;
+	console.log('########## [Set Linux Path] ##########');
+	console.log(linux_path);
+	fs.writeFileSync('.linux_path',linux_path,{encoding:ENCODING});
+	fs.writeFileSync(HOME + '/.profile','export PATH=' + linux_path,{encoding:ENCODING});
+	console.log();
+})();
 
-// set git bash PATH
-fs.writeFileSync(HOME + '/.profile','export PATH=' + linux_path,{encoding:ENCODING});
+(function(){
+	console.log('########## [Set Env Vars] ##########');
+	var lines = readLines(__dirname + '/vars');
+	var n = lines.length;
+	var patt = /^([^=]+)=(.*)/;
+	var vars = [
+		{
+			name:"TEMP",
+			value:HOME + "\\AppData\\Local\\Temp"
+		}, {
+			name:"TMP",
+			value:HOME + "\\AppData\\Local\\Temp"
+		}
+	];
+	for(var i = 0;i < n;i++){
+		var line = trim(lines[i]);
+		if(patt.test(line)){
+			var match = line.match(patt);
+			var name = trim(match[1]);
+			var value = trim(match[2]);
+			vars.push({name:name,value:value});
+		}
+	}
+
+	var n = vars.length;
+	for(var i = 0;i < n;i++){
+		var v = vars[i];
+		console.log(v.name + '=' + '"' + v.value + '"');
+		setenv(v.name,v.value);
+	}
+})();
