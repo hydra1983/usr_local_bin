9843e20f93fe5f8095179422e73c09a777c59c8c
hydra1983 <hydra1983@gmail.com>
2014-01-07 03:11:42 +0800

    finish jdk2z

--- extra/jdk2z/jdk2z.bat
+++ extra/jdk2z/jdk2z.bat
@@ -14,15 +14,20 @@ REM ########################################
 		If Not Exist "%jdk_installer_full_path%" Goto unexisting_source
 		For /f "delims=" %%i In ("%jdk_installer_path%") Do Set jdk_installer_name=%%~ni
 
-		If [%2]==[] (Set target_zip_path=%cd%\%jdk_installer_name%.zip) Else (Set target_zip_path=%2)
-		For /f "delims=" %%i In ("%target_zip_path%") Do Set target_zip_path=%%~i
-		For /f "delims=" %%i In ("%target_zip_path%") Do Set target_zip_ext=%%~xi
-		If "%target_zip_ext%"=="" (
-			Set target_zip_path=!target_zip_path!\%jdk_installer_name%.zip
+		If [%2]==[] (Set target_archive_path=%cd%\%jdk_installer_name%.zip) Else (Set target_archive_path=%2)
+		For /f "delims=" %%i In ("%target_archive_path%") Do Set target_archive_path=%%~i
+		For /f "delims=" %%i In ("%target_archive_path%") Do Set target_archive_ext=%%~xi
+		If "%target_archive_ext%"=="" (
+			Set target_archive_ext=zip
+			Set target_archive_path=!target_archive_path!\%jdk_installer_name%.%target_archive_ext%
+		) Else (
+			Set target_archive_ext=!target_archive_ext:~1!
 		)
 
-		Echo [INFO] Source:%jdk_installer_path%
-		Echo [INFO] Target:%target_zip_path%
+		For /f "delims=" %%i In ("%target_archive_path%") Do Set target_archive_full_path=%%~fi
+
+		Echo [INFO] Source:%jdk_installer_full_path%
+		Echo [INFO] Target:%target_archive_full_path%
 
 		Set 7z="%~dp07z\7z.exe"
 		For /f "usebackq tokens=*" %%i In (`Cscript "%~dp0uuidgen.vbs"`) Do Set uuid=%%i
@@ -30,7 +35,7 @@ REM ########################################
 		Set outputdir=%tmpdir%\%uuid%
 
 		Echo [INFO] Extract to "%outputdir%"
-		!7z! x -y -o"%outputdir%" "%jdk_installer_path%" > NUL
+		!7z! x -y -o"%outputdir%" "%jdk_installer_full_path%" > NUL
 
 		If Exist "%outputdir%\.rsrc" call :process_jdk6
 		If Exist "%outputdir%\tools" call :process_jdk7
@@ -64,9 +69,9 @@ Goto eof
 		Echo [INFO] Unpack jars in "%cd%"
 		For /r %%i In (*.pack) Do .\bin\unpack200 -r "%%i" "%%~di%%~pi%%~ni.jar" > NUL
 
-		Echo [INFO] Build "%target_zip_path%"
-		If Exist "%target_zip_path%" Del "%target_zip_path%"
-		!7z! a -tzip "%target_zip_path%" * -r > NUL
+		Echo [INFO] Build "%target_archive_full_path%"
+		If Exist "%target_archive_full_path%" Del "%target_archive_full_path%"
+		!7z! a -t%target_archive_ext% "%target_archive_full_path%" * -r > NUL
 	popd	
 Goto eof
 
@@ -74,12 +79,35 @@ Goto eof
 Goto usage
 
 :unexisting_source
-	Echo File "%jdk_installer_path%" not exists
+	Echo File "%jdk_installer_full_path%" not exists
 Goto error
 
 :usage
-	Echo Usage:
-	Echo %~n0 jdk_installer_path [target_zip_path]
+	Echo Build archive files from executable installer of oracle jdk 
+	Echo.
+	Echo Usage: %~n0 jdk_installer_path [target_archive_path]
+	Echo. 
+	Echo     Example 1:
+	Echo.
+	Echo         jdk2z C:\Users\{User}\Desktop\jdk-6u45-windows-i586.exe
+	Echo         D:\usr\local\lib\oraclejdk-1.6.0_45-x32
+	Echo.
+	Echo     You will find "jdk-6u45-windows-i586.zip" in directory
+	Echo     "D:\usr\local\lib\oraclejdk-1.6.0_45-x32"
+	Echo. 
+	Echo     Example 2:
+	Echo.
+	Echo         jdk2z C:\Users\{User}\Desktop\jdk-6u45-windows-i586.exe
+	Echo         D:\usr\local\lib\oraclejdk-1.6.0_45-x32.7z
+	Echo.
+	Echo     You will find "oraclejdk-1.6.0_45-x32.7z" in directory
+	Echo     "D:\usr\local\lib"
+	Echo. 
+	Echo     Example 3:
+	Echo.
+	Echo         jdk2z jdk-6u45-windows-i586.exe oraclejdk-1.6.0_45-x32.7z
+	Echo.
+	Echo     You will find "oraclejdk-1.6.0_45-x32.7z" in current directory
 Goto eof
 
 REM ########################################
@@ -90,28 +118,3 @@ REM ########################################
 
 :eof
  	Exit /b 0
-==================================================
-JDK is not available as a portable zip unfortunately. However, you can:
-
-Create working JDK directory ("C:\JDK" in this case)
-Download latest version of JDK from oracle (for example "jdk-7u7-windows-i586.exe")
-Download and install 7-zip or download 7-zip portable version if you are not administrator
-With 7-zip extract all the files from "jdk-7u7-windows-i586.exe" in directory "C:\JDK"
-In command shell (cmd.exe) do the following:
-
---> change directory to directory C:\JDK.rsrc\JAVA_CAB10
-
---> execute command: extrac32 111
-
-Unpack C:\JDK.rsrc\JAVA_CAB10\tools.zip with 7-zip
-
-In command shell (cmd.exe) do the following:
-
---> change directory to C:\JDK.rsrc\JAVA_CAB10\tools\
-
---> execute command: for /r %x in (*.pack) do .\bin\unpack200 -r "%x" "%~dx%~px%~nx.jar" (this will convert all pack files into jar)
-
-Copy whole directory and all subdir of c:\JDK.rsrc\JAVA_CAB10\tools" where you want your JDK to be and setup manually JAVA_HOME and PATH to point to your JDK dir and its BIN subdir.
-
-Thats all. After this you'll be able at least to use javac.exe
-========================================
